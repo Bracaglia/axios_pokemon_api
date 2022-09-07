0a777f8b45dc75b357bab8774c358c3341ecd06b
Bracaglia <shaws0598@gmail.com>
2022-09-07 13:11:21 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Pokemon from './components/Pokemon' // NEEDED WHEN YOU MAKE A NEW ROUTE!!!!!
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokemon />
+    </div>
+  );
+}
+
+export default App;
